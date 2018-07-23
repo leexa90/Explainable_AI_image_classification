b08c2919817e28f2588d16b16b29ec2b3dcb5613
leexa <lee.x.a90@gmail.com>
2018-07-23 14:47:12 +0800

    colabs

--- run_equal81_mnist.py
+++ run_equal81_mnist.py
@@ -77,9 +77,9 @@ model.load_weights('mnist.h5') #trained with astar super computer for 30mins
 X_train = mnist.train.images
 X_train = np.reshape(X_train,(X_train.shape[0],28,28,1))
 X_val = mnist.test.images
-X_val = np.reshape(X_val,(X_val.shape[0],28,28,1))[::11]
+X_val = np.reshape(X_val,(X_val.shape[0],28,28,1))[::1]
 y_train = mnist.train.labels
-y_val = mnist.test.labels[::11]
+y_val = mnist.test.labels[::1]
 
 
 pred= model.predict(X_val)
@@ -114,17 +114,17 @@ if filename+'.gz' in os.listdir('.'):
     dictt= np.load(gzip.GzipFile(filename+'.gz')).item()
 print (len(X_val))
 for i in range(len(X_val))[::]:
-    if i in dictt.keys():
+    if i in dictt.keys() and i !=0 : 
        print i,'in dictt.keys()'
        continue
-    temp = [[],[],[],[]]
+    temp = [[],]*10
     feature_names={'0':'late blight','1':'leaf mold','2':'leaf spot','3':'healthy'}
     img = X_val[i]
     import copy
     img_orig = copy.deepcopy(img)
     segments_slic = (slic(img, n_segments=34, compactness=10, sigma=0.5))
-    preds_1  =model.predict(X_val[i:i+1])
-    if np.argmax( preds_1[0]) == np.argmax(y_val[i]):
+    preds_1  =model.predict(X_val[i:i+1]) 
+    if np.argmax( preds_1[0]) == np.argmax(y_val[i]) and i !=0:
        print np.argmax( preds_1[0]) , np.argmax(y_val[i])
        continue
     max_val = np.max(segments_slic)+1 #segment is from 0 to n-1
@@ -164,12 +164,12 @@ for i in range(len(X_val))[::]:
     W_mat = np.diag(map(shapley_kernel,sample_index)).astype(np.float32)
     X= np.stack([X_simple,]*1,-1)*np.stack([img,]*len(X_simple))+.0*np.mean(img)*(np.stack([X_simple,]*1,-1)==0).astype(np.float32)
     gc.collect()
-    Y_pred = model.predict(X,batch_size = 512/4)
+    Y_pred = model.predict(X,batch_size = 512/4)  
 #    Y_pred1 = model.predict(X[1::2],batch_size = 512/4)
 #    Y_pred = np.concatenate([Y_pred0,Y_pred1],0)
     print Y_pred.shape
     print (Y_pred[0])
-    Y_pred = np.log(Y_pred/(1-Y_pred))-np.log(0.333) #linearize probs
+    Y_pred = np.log((0.000001+Y_pred)/(1.000001-Y_pred))#-np.log(1./9) #linearize probs
     print len(Y_pred)
     print (preds_1)
     #φ = (X T W X) −1 X T W y
@@ -182,11 +182,11 @@ for i in range(len(X_val))[::]:
       np.save(filename,dictt)
       print len(dictt)
 np.save(filename,dictt)
-for i  in range(len(X_val))[::2]:
+for i  in range(len(X_val))[::]:
     if i in dictt.keys():
        print i,'in dictt.keys()'
        continue
-    temp = [[],[],[],[]]
+    temp = [[],]*10
     feature_names={'0':'late blight','1':'leaf mold','2':'leaf spot','3':'healthy'}
     img = X_val[i]
     import copy
@@ -199,31 +199,30 @@ for i  in range(len(X_val))[::2]:
     vec = np.array(range(max_val))
     Continue=False
     num =10000
-    num_comb = 0
-    sample_index = []
-    counter = 0
-    while num_comb < num:
-        num_comb += 1*scipy.misc.comb(max_val,counter)
-        counter += 1
-    for num_of_chosen in range(1,counter):
-        if num_of_chosen < counter-1:
-            sample_index += list(itertools.combinations(range(max_val), max_val-num_of_chosen))
+    #num_comb = 0
+    #sample_index = []
+    #counter = 0
+    #while num_comb < num:
+    #    num_comb += 1*scipy.misc.comb(max_val,counter)
+    #    counter += 1
+    #for num_of_chosen in range(1,counter):
+    #    if num_of_chosen < counter-1:
+    #        sample_index += list(itertools.combinations(range(max_val), max_val-num_of_chosen))
             #sample_index += list(itertools.combinations(range(max_val), num_of_chosen))
-        else:
-             sample_index += list(itertools.combinations(range(max_val), max_val
--num_of_chosen))[::11]
+    #    else:
+    #         sample_index += list(itertools.combinations(range(max_val), max_val-num_of_chosen))[::3]
 
             #random_array = np.random.randint(0,2,size=(num-len(sample_index),max_val))
             #sample_index += map(lambda x : np.reshape(x,len(x)),
             #                   map(lambda x : np.argwhere(x == 1),random_array))
-    X_simple = np.array(map(lambda x : 1*np.isin(segments_slic,x),sample_index)).astype(np.float32)
-    random_array = []
-    for l2 in sample_index:
-        temp2 = np.zeros(max_val)
-        for j2 in l2:
-            temp2[j2] = 1
-        random_array += [temp2,]
-    random_array  = np.stack(random_array,0).astype(np.float32)
+    #X_simple = np.array(map(lambda x : 1*np.isin(segments_slic,x),sample_index)).astype(np.float32)
+    #random_array = []
+    #for l2 in sample_index:
+    #    temp2 = np.zeros(max_val)
+    #    for j2 in l2:
+    #        temp2[j2] = 1
+    #    random_array += [temp2,]
+    #random_array  = np.stack(random_array,0).astype(np.float32)
     print (X_simple.shape)
     gc.collect()
     import scipy
@@ -239,7 +238,7 @@ for i  in range(len(X_val))[::2]:
 #    Y_pred = np.concatenate([Y_pred0,Y_pred1],0)
     print Y_pred.shape
     print (Y_pred[0])
-    Y_pred = np.log(Y_pred/(1-Y_pred))  - np.log(0.333)#linearize probs
+    Y_pred = np.log((0.000001+Y_pred)/(1.000001-Y_pred))  #- np.log(1.0/9)#linearize probs
     print len(Y_pred)
     print (preds_1)
     #φ = (X T W X) −1 X T W y
